6aca975a5ecafd590d85ec66573be78845f1dbd3
aura2025 <aura.pelaez@correounivalle.edu.co>
2025-12-09 09:49:12 -0500

    Actualizacion controlador

--- App.java
+++ App.java
@@ -4,8 +4,8 @@ import Vista.VistaGui;
 import Vista.Vista;
 public class App {
     public static void main(String[] args) {
-        VistaTerminal vista = new VistaTerminal();
-       // VistaGui vista = new VistaGui();
+       // VistaTerminal vista = new VistaTerminal();
+        VistaGui vista = new VistaGui();
 
         Controlador controlador = new Controlador(vista);
         controlador.iniciar();
